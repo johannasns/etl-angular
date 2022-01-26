f94607d2c119dc129d12261657e0daf72ecea97e
johannasonsini <johannasonsini3@gmail.com>
2022-01-26 15:06:07 +0100

    directives

--- src/app/directives/highlight.directive.ts
+++ src/app/directives/highlight.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  private prev!: string;
+  @Input() public color!: string; // unica proprietà rilevante per determinare lo stato della direttiva
 
+  constructor(private _el: ElementRef<HTMLElement>) { 
+     console.log("Ok");
+    }
+  
+    
+  @HostListener('mouseover') 
+  public handleMouseOver(){
+      console.log("Mouse over")
+      this.prev = this._el.nativeElement.style.backgroundColor;
+      this._el.nativeElement.style.backgroundColor = this.color;
+  }
+
+  @HostListener('mouseleave')
+  public handleMouseLeave() {
+    this._el.nativeElement.style.backgroundColor = this.prev;
+  }
 }
+
