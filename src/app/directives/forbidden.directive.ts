923a2047b59e0f77a2a92d1d208099876c204548
johanna sonsini <johannasonsini3@gmail.com>
2022-02-02 21:18:15 +0100

    esercizio che dubito sia giusto

--- src/app/directives/forbidden.directive.ts
+++ src/app/directives/forbidden.directive.ts
@@ -1,16 +1,27 @@
-import { Directive } from '@angular/core';
-import { AbstractControl, ValidationErrors, Validator } from '@angular/forms';
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
 import { forbiddenValidator } from '../validators/string.validators';
 
 @Directive({
+  providers: [{
+    multi: true,
+    provide: NG_VALIDATORS,
+    // Se esiste un'istanza della classe, utilizza quella
+    useExisting: ForbiddenDirective
+  }],
   selector: '[appForbidden]'
 })
-export class ForbiddenDirective implements Validator { 
+export class ForbiddenDirective implements Validator {
 
   constructor() { }
 
+@Input() private _invalidWords!: string;
+
   public validate(control: AbstractControl): ValidationErrors | null {
-      forbiddenValidator()
+      // const fnInterna = forbiddenValidator('boh', 'bah');
+      // return fnInterna(control);
+
+      return forbiddenValidator(this._invalidWords)(control);
   }
 
 }
--- src/app/validators/string.validators.ts
+++ src/app/validators/string.validators.ts
@@ -1,3 +1,35 @@
-export function forbiddenValidator() {
-    
-}
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+// Fn esterna
+//   - Raccoglie dati utili ai fini della validazione
+//   - Restituisce la funzione di validazione interna,
+//     quella utilizzata di fatto per validare il campo del form
+export const forbiddenValidator = (...values: string[]): ValidatorFn => {
+    // La funzione interna che viene restituita da quella esterna
+    // Questa verrà usata da Angular per eseguire la validazione
+    // sul campo in input / gruppo
+    //
+    // Quando viene invocata da Angular, le viene passato il Control
+    // istanziato sull'elemento del Form
+    return (c: AbstractControl): ValidationErrors | null => {
+        for (const value of values) {
+            // Se quanto digitato dall'utente (c.value) coincide
+            // con uno degli elementi nell'array (una delle parole non ammesse)
+            // restituisco un errore
+            if (value === c.value) {
+                return {
+                    // La chiave da usare nell'oggetto di errore
+                    // dovrebbe chiamarsi come la funzione di validazione
+                    //
+                    // Questa chiave è l'elemento che viene cercato
+                    // con il metodo "hasError"
+                    forbidden: true
+                };
+            }
+        }
+
+        // Nel caso in cui non si debba restituire un errore,
+        // Angular impone la restituzione del valore null
+        return null;
+    };
+};
