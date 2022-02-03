70ec45294641cd544280598d89bac4a827cec8ac
johanna sonsini <johannasonsini3@gmail.com>
2022-02-03 23:17:50 +0100

    commenti

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,14 +8,20 @@ import { NgForm } from '@angular/forms';
 })
 export class LoginComponent implements OnInit {
 
+  //facciamo una prorpeità per ogni campo in input
+  //queste proprietà accoglieranno tutte le informazioni del campo in input
   public email: string = '';
   public password: string = '';
 
   constructor() { }
-
+  
+  //dentro le () mi vado a passare il parametro dell'NgForm 
   public handleSubmit(form: NgForm): void {
-    if (form.invalid) {
-      return;
+    //faccio un if per dirmi che se il form non è valido , "esci".
+    if (form.invalid){
+      //invalid mi serve per bloccare "delle chiamate", quindi si bloccherà (sarà true) ogni volta che 
+      //un "required" di un campo in input non verrà rispettato.
+    return;
     }
     
     console.log(this.email, this.password);
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -23,3 +23,19 @@ export class UserComponent implements OnInit {
   }
 
 }
+
+    // prelevo l'id + chiamata al backend
+    // param e query params sono DEPRECATE perchè delle vecchie versioni di "ANGULAR"
+    // bisogna usare quelli con "Map" finale
+    // questa notazione è un Observable che emetterà degli oggetti , quindi ci tocca fare un subscribe.
+    // e' un observable , perchè essendo Angular sviluppato con rxjs, ci costringe a fare tutto in asincrono
+    // anche se questo Observable fa una sola chiamata e non servirebbe una funziona asincrona.
+    // dentro le () ci prendiamo l'elemento che "emette" con un parametro p
+    // l'elemento param map ha 3 metodi
+    // get --> parametro di una stringa che voglio recuperare.
+    // getAll --> mi da un array con tutti i dati
+    // has --> vuole una stringa, però ci dice solo se quell'elemtno esiste o no (restituisce un booleano)
+    // keys --> un array di stringhe con solo chiavi e non valori
+    // il console.log mi restituisce una stringa, perchè ci riferiamo al percorso nell'url
+    // this._route.paramMap.subscribe( p => console.log(p.get('id'))); 
+    //mi aggancio con ".pipe" al paramMap, che collega i due "stream" (le due linee temporali)
